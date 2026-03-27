2d07102fcee4632897175d3d362d831baf933f17
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2026-03-27 14:06:45 -0700

    add tracker struct

--- main.cu
+++ main.cu
@@ -7,34 +7,25 @@
 
 int main(void){
 
-    int Max_Tracks=2000;
-    //data structure and device correspondings
-    //track IDs
-    int* track_id=new int(Max_Tracks);
-    int* d_track_id;
-    //state variables
-    float * state = new float(Max_Tracks*7);
-    float * d_state;
-    //state covariance matrix
-    float * Pcov = new float(Max_Tracks*49);
-    float*d_Pcov;
-    //age of each tracks
-    int* age = new int(Max_Tracks);
-    int* d_age;
-    //consecutive hit for each track
-    int* hit_streak = new int(Max_Tracks);
-    int* d_hit_streak;
-    //consecutive hit for each track
-    int* time_last_update = new int(Max_Tracks);
-    int* d_time_last_update;
-    // number of active tracks
-    int count;
-    
+    //set a max expected detection 2000
+    int Max_Tracks=1000;
+    int Max_detection = 1000;
+    //set state variable and detection spec
+    int m=4;     //detection
+    int n=7;     //state variable
 
-    //
-    float* K=(float*)malloc(sizeof(float)*7*4*Max_Tracks);
-    float* H=(float*)malloc(sizeof(float)*7*4);
-    float* R=(float*)malloc(sizeof(float)*4*4);
+    tracker* d_tracker;
+    cudaMalloc(&d_tracker,sizeof(tracker));
+    tracker tracker1(Max_Tracks,Max_detection,m,n);
+    tracker1.allocateOnDevice();
+    tracker* tracker2 = (tracker*)malloc(sizeof(tracker));
+    cudaMemcpy(d_tracker,&tracker1,sizeof(tracker),cudaMemcpyHostToDevice);
+    cudaMemcpy(tracker2,d_tracker,sizeof(tracker),cudaMemcpyDeviceToHost);
+    printf("max tracks: %d",tracker2->Max_Tracks);
+    tracker1.freeOnDevice();
+    cudaFree(d_tracker);
+    free(tracker2);
+
 
     return 0;
 }
--- sort_lib.h
+++ sort_lib.h
@@ -3,6 +3,54 @@
 
 #include <cuda_runtime.h>
 
+//tracker struct wrapper
+typedef struct tracker{
+    int Max_Tracks;int Max_detection;
+    int m;int n;    
+    int* d_track_id;    //track IDs
+    float * d_state;    //state variables
+    float*d_Pcov;       //state covariance matrix
+    int* d_age;         //age of each tracks
+    int* d_hit_streak;  //consecutive hit for each track
+    int* d_activetracks; // number of active tracks
+    float* d_Z;         // measurement/dection buffer, m*N
+    int d_currentdetections; // number of detection get
+    float* d_K;float* d_H;float* d_R; //matrix for kalman gain
+
+    //constructor
+    tracker(int MT=2000,int MD=2000, int measure=4, int state=7):
+        Max_Tracks(MT),Max_detection(MD), m(measure),n(state) {}
+
+    //allocate device memory for member matrix
+    void allocateOnDevice(){
+        cudaMalloc((void**)&d_track_id,sizeof(int)*Max_Tracks);
+        cudaMalloc((void**)&d_state,sizeof(float)*Max_Tracks*n);
+        cudaMalloc((void**)&d_Pcov,sizeof(float)*Max_Tracks*n*n);
+        cudaMalloc((void**)&d_age,sizeof(int)*Max_Tracks);
+        cudaMalloc((void**)&d_hit_streak,sizeof(int)*Max_Tracks);
+        cudaMalloc((void**)&d_activetracks,sizeof(int));
+        cudaMalloc((void**)&d_Z,sizeof(float)*Max_detection*m);
+        cudaMalloc((void**)&d_K,sizeof(float)*n*m*Max_Tracks);
+        cudaMalloc((void**)&d_H,sizeof(float)*n*m);
+        cudaMalloc((void**)&d_R,sizeof(float)*m*m);
+    }
+    //free the device tracker object matrix
+    void freeOnDevice(){
+        cudaFree(d_track_id);
+        cudaFree(d_state);
+        cudaFree(d_Pcov);
+        cudaFree(d_age);
+        cudaFree(d_hit_streak);
+        cudaFree(d_activetracks);
+        cudaFree(d_Z);
+        cudaFree(d_K);
+        cudaFree(d_H);
+        cudaFree(d_R);
+        
+    }
+
+}tracker;
+
 
 
 // A wrapper function that calls the GPU kernel
