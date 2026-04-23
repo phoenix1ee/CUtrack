65bc6d48a94dd47e94281bf9abbe1e936ff9e2df
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2026-04-23 14:58:16 -0700

    update test input

--- test_detection/test_detection2.cu
+++ test_detection/test_detection2.cu
@@ -38,6 +38,7 @@ try {
     size_t detector_output_size = detector.output_shapes_size[0]*sizeof(float);
     cudaMalloc((void**)&detector_output,detector_output_size);
 
+    //create the tensor for the model
     detector.create_input_tensor(preprocess_output,data_size_after_preprocess);
     detector.create_output_tensor(detector_output,detector_output_size);
     detector.bind_tensors();
@@ -54,12 +55,52 @@ try {
     std::cout<<"finished preprocessing"<<std::endl;
     
     detector.run();
-
-    // Get a pointer to the results
+    //Get a pointer to the results
     //float* d_raw_output = output_tensor[0].GetTensorMutableData<float>();
+    
+    float* d_processed_detection;
+    size_t Num_raw_detection = detector.output_shapes[0][2];//number of detections 8400
+    int height_raw_detection = detector.output_shapes[0][1];//number of 0:3 coordinates, 4-83, score of each class
+    cudaMalloc((void**)&d_processed_detection,Num_raw_detection*sizeof(float)*5);
+
+    int* d_raw_class_id;
+    cudaMalloc((void**)&d_raw_class_id,Num_raw_detection*sizeof(int));
+
+    int* d_final_class_id;
+    cudaMalloc((void**)&d_final_class_id,Num_raw_detection*sizeof(int));
+
+    int* d_detection_count;
+    cudaMalloc((void**)&d_detection_count,sizeof(int));
+
+    NMS(detector_output,d_raw_class_id,Num_raw_detection,height_raw_detection,d_processed_detection,d_final_class_id,d_detection_count);
+    
+    float* testoutput = (float*)malloc(Num_raw_detection*sizeof(float)*5);
+    cudaMemcpy(testoutput,d_processed_detection,Num_raw_detection*sizeof(float)*5,cudaMemcpyDeviceToHost);
+
+    float* testoutputraw = (float*)malloc(Num_raw_detection*sizeof(float)*5);
+    cudaMemcpy(testoutputraw,detector_output,Num_raw_detection*sizeof(float)*5,cudaMemcpyDeviceToHost);
+
+    int* testclassoutput = (int*)malloc(Num_raw_detection*sizeof(int));
+    cudaMemcpy(testclassoutput,d_final_class_id,sizeof(int)*Num_raw_detection,cudaMemcpyDeviceToHost);
+
+    int totalcount[1]={0};
+    cudaMemcpy(totalcount,d_detection_count, sizeof(int),cudaMemcpyDeviceToHost);
+    std::cout<<"total detections:"<<totalcount[0]<<std::endl;
+    write2DArrayToFile(testoutputraw,5,Num_raw_detection,"rawdetection.txt");
+    write2DArrayToFile(testoutput,5,Num_raw_detection,"sorteddetection.txt");
+    write2DArrayToFileInt(testclassoutput,1,Num_raw_detection,"detectedclass.txt");
+
     cudaFree(preprocess_input);
     cudaFree(preprocess_output);
     cudaFree(detector_output);
+    cudaFree(d_processed_detection);
+    cudaFree(d_raw_class_id);
+    cudaFree(d_final_class_id);
+    cudaFree(d_detection_count);
+
+    free(testoutput);
+    free(testoutputraw);
+    free(testclassoutput);
     std::cout<<"I am done"<<std::endl;
     } catch (const Ort::Exception& e) {
         printf("ONNX Error: %s\n", e.what());
