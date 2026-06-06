ff729b9365e1941d9ab0c8f5d686566f4e4b1164
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2026-06-05 22:27:04 -0700

    Delete unused CPU functions

--- include/hungarian_cpu_vectorized.h
+++
@@ -1,7 +0,0 @@
-#ifndef HUNGARIAN_CPU_VECTORIZED_H
-#define HUNGARIAN_CPU_VECTORIZED_H
-
-// Declarations that both NVCC and G++ can see
-void reduction_avx(float* input, int width, int height);
-
-#endif
